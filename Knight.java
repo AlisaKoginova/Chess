878ac7b88125d767c51665ff73a9585950c53130
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2018-12-07 08:05:46 +0200

    Create Knight.java

---
+++ Knight.java
@@ -0,0 +1,20 @@
+public class Knight extends Piece{
+
+    public Knight(boolean accessible, int x, int y) {
+        super(accessible, x, y);
+    }
+
+    @Override
+    public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
+        if(super.isValid(board, fromX, fromY, toX, toY) == false)
+            return false;
+
+        if(toX != fromX - 1 && toX != fromX + 1 && toX != fromX + 2 && toX != fromX - 2)
+            return false;
+        if(toY != fromY - 2 && toY != fromY + 2 && toY != fromY - 1 && toY != fromY + 1)
+            return false;
+
+        return true;
+    }
+
+}
