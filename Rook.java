6e450ccc38549ff4925a8f06515cd38c3554881f
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2018-12-18 19:55:31 +0200

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,18 +1,62 @@
-public class Rook extends Piece{
 
-    public Rook(boolean accessible, int x, int y) {
-        super(accessible, x, y);
-    }
-
-
-    @Override
-    public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
-        if(super.isValid(board, fromX, fromY, toX, toY) == false)
-            return false;
-        if(toX == fromX)
-            return true;
-        if(toY == fromY)
-            return true;
-        return false;
+public class Rook extends Game{
+	
+	public static String posible(int i) {
+        String list="", oldPiece;
+        int r=i/8, c=i%8;
+        int temp=1;
+        for (int j=-1; j<=1; j+=2) {
+            try {
+                while(" ".equals(chessBoard[r][c+temp*j]))
+                {
+                    oldPiece=chessBoard[r][c+temp*j];
+                    chessBoard[r][c]=" ";
+                    chessBoard[r][c+temp*j]="R";
+                    if (kingSafe()) {
+                        list=list+r+c+r+(c+temp*j)+oldPiece;
+                    }
+                    chessBoard[r][c]="R";
+                    chessBoard[r][c+temp*j]=oldPiece;
+                    temp++;
+                }
+                if (Character.isLowerCase(chessBoard[r][c+temp*j].charAt(0))) {
+                    oldPiece=chessBoard[r][c+temp*j];
+                    chessBoard[r][c]=" ";
+                    chessBoard[r][c+temp*j]="R";
+                    if (kingSafe()) {
+                        list=list+r+c+r+(c+temp*j)+oldPiece;
+                    }
+                    chessBoard[r][c]="R";
+                    chessBoard[r][c+temp*j]=oldPiece;
+                }
+            } catch (Exception e) {}
+            temp=1;
+            try {
+                while(" ".equals(chessBoard[r+temp*j][c]))
+                {
+                    oldPiece=chessBoard[r+temp*j][c];
+                    chessBoard[r][c]=" ";
+                    chessBoard[r+temp*j][c]="R";
+                    if (kingSafe()) {
+                        list=list+r+c+(r+temp*j)+c+oldPiece;
+                    }
+                    chessBoard[r][c]="R";
+                    chessBoard[r+temp*j][c]=oldPiece;
+                    temp++;
+                }
+                if (Character.isLowerCase(chessBoard[r+temp*j][c].charAt(0))) {
+                    oldPiece=chessBoard[r+temp*j][c];
+                    chessBoard[r][c]=" ";
+                    chessBoard[r+temp*j][c]="R";
+                    if (kingSafe()) {
+                        list=list+r+c+(r+temp*j)+c+oldPiece;
+                    }
+                    chessBoard[r][c]="R";
+                    chessBoard[r+temp*j][c]=oldPiece;
+                }
+            } catch (Exception e) {}
+            temp=1;
+        }
+        return list;
     }
 }
