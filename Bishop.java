38840030d36cfb24610e2aa0e38fa5f615f57b23
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2018-12-18 19:50:14 +0200

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,18 +1,38 @@
-public class Bishop extends Piece{
 
-    public Bishop(boolean available, int x, int y) {
-        super(accessible, x, y);
-    }
-
-    @Override
-    public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
-        if(super.isValid(board, fromX, fromY, toX, toY) == false)
-            return false;
-
-        if(toX - fromX == toY - fromY)
-            return true;
-
-        return false;
-    }
-
+public class Bishop extends Game{
+	 public static String posible(int i) {
+	        String list="", oldPiece;
+	        int r=i/8, c=i%8;
+	        int temp=1;
+	        for (int j=-1; j<=1; j+=2) {
+	            for (int k=-1; k<=1; k+=2) {
+	                try {
+	                    while(" ".equals(chessBoard[r+temp*j][c+temp*k]))
+	                    {
+	                        oldPiece=chessBoard[r+temp*j][c+temp*k];
+	                        chessBoard[r][c]=" ";
+	                        chessBoard[r+temp*j][c+temp*k]="B";
+	                        if (kingSafe()) {
+	                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+	                        }
+	                        chessBoard[r][c]="B";
+	                        chessBoard[r+temp*j][c+temp*k]=oldPiece;
+	                        temp++;
+	                    }
+	                    if (Character.isLowerCase(chessBoard[r+temp*j][c+temp*k].charAt(0))) {
+	                        oldPiece=chessBoard[r+temp*j][c+temp*k];
+	                        chessBoard[r][c]=" ";
+	                        chessBoard[r+temp*j][c+temp*k]="B";
+	                        if (kingSafe()) {
+	                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+	                        }
+	                        chessBoard[r][c]="B";
+	                        chessBoard[r+temp*j][c+temp*k]=oldPiece;
+	                    }
+	                } catch (Exception e) {}
+	                temp=1;
+	            }
+	        }
+	        return list;
+	    }
 }
